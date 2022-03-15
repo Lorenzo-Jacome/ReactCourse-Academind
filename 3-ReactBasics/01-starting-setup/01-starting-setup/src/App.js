df2914c17631ce25782e65ce80dbaf497f2eae52
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2022-03-15 12:07:35 -0600

    Update on lecture of today

--- 3-ReactBasics/01-starting-setup/01-starting-setup/src/App.js
+++ 3-ReactBasics/01-starting-setup/01-starting-setup/src/App.js
@@ -1,9 +1,41 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: 'e0', title: 'Game Pass', amount: 247.99, date: new Date(2021, 2, 28), },
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem 
+        title={expenses[0].title} 
+        amount={expenses[0].amount} 
+        month={expenses[0].date.toLocaleString('en-US', {month: 'long'})}
+        day={expenses[0].date.toLocaleString('en-US', {day: '2-digit'})}
+        year={expenses[0].date.getFullYear()}
+      ></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- 3-ReactBasics/01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ 3-ReactBasics/01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,23 @@
-const ExpenseItem = () =>{
-    return <h2>Expense Item</h2>
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+    const year = props.year;
+    const month = props.month;
+    const day = props.day;
+
+    return (
+        <div className="expense-item">
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
